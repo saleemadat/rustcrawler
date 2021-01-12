3768e257ef2bab47caddcee978aed7606f9b9650
saleem <saleemadat@gmail.com>
2021-01-12 20:38:30 +0000

    chore: added main.rs

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use html5ever::tokenizer::{
+    BufferQueue, Tag, TagKind, Token, TokenSink, TokenSinkResult, Tokenizer, TokenizerOpts,
+};
+
+use std::borrow::Borrow;
+use url::{ParseError, Url};
+
+fn main() {
+    print!("{}", "hello world");
+}
